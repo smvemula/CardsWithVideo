181d31411648f38759d81c8473b35080ba259564
Manoj Vemula <manoj@MacBook-Pro-4.local>
2022-11-15 09:53:54 -0800

    first commit with project default files

---
+++ CardsWithVideo/CardsWithVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsWithVideoApp.swift
+//  CardsWithVideo
+//
+//  Created by Manoj Vemula on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CardsWithVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
