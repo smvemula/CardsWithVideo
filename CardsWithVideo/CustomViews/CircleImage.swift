79b32600ea54dd00fa97d802f464251d8925b184
Manoj Vemula <manoj@MacBook-Pro-4.local>
2022-11-15 12:24:47 -0800

    wrapped up adding list views using SwiftUI

---
+++ CardsWithVideo/CustomViews/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  CardsWithVideo
+//
+//  Created by Manoj Vemula on 11/15/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
---
+++ CardsWithVideo/CustomViews/LandmarkRow.swift
@@ -0,0 +1,39 @@
+//
+//  LandmarkRow.swift
+//  CardsWithVideo
+//
+//  Created by Manoj Vemula on 11/15/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: ModelData().landmarks[0])
+            LandmarkRow(landmark: ModelData().landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
